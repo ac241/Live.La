1e51976251d04f53437f71a00d2097a60da56f7c
acel <clanvick@gmail.com>
2020-07-15 20:24:15 +0800

    高亮本日赛程

--- app/src/main/java/com/acel/streamlivetool/platform/anchor_additional/GetLPLMatchAction.kt
+++ app/src/main/java/com/acel/streamlivetool/platform/anchor_additional/GetLPLMatchAction.kt
@@ -1,5 +1,8 @@
 package com.acel.streamlivetool.platform.anchor_additional
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.util.Log
 import com.acel.streamlivetool.R
 import com.acel.streamlivetool.base.MyApplication
 import com.acel.streamlivetool.net.RetrofitUtils
@@ -8,8 +11,11 @@ import com.acel.streamlivetool.util.TextUtil
 import com.google.gson.Gson
 import okhttp3.Request
 import java.lang.StringBuilder
+import java.text.SimpleDateFormat
+import java.util.*
 
 class GetLPLMatchAction : AdditionalActionInterface {
+    @SuppressLint("ResourceType")
     override fun get(): String {
         val html = RetrofitUtils.okHttpClient.newCall(
             Request.Builder().get().url("https://www.scoregg.com/match_pc?tournamentID=172").build()
@@ -23,8 +29,15 @@ class GetLPLMatchAction : AdditionalActionInterface {
             )
             val lplMatch = Gson().fromJson(jsonStr, LPLMatch::class.java)
             val matches = StringBuilder()
+            val currentTime = System.currentTimeMillis()
+            val mdFormat = SimpleDateFormat("MM-dd", Locale.CHINA)
+            val color = MyApplication.application.resources.getString(R.color.colorPrimary).trim().replaceFirst("FF", "",  true)
             lplMatch.this_week_match.forEach {
-                matches.append("<li>${it.start_date} ${it.start_time} ${it.team_a_name} ${it.team_b_name}</li>")
+                val date = it.start_time.split(" ")[0]
+                val dateMatch = date == mdFormat.format(currentTime)
+                matches.append(
+                    "<li ${if (dateMatch) "style='color:$color'" else ""}>${if (dateMatch) "<b>" else ""}${it.start_date} ${it.start_time} ${it.team_a_name} vs ${it.team_b_name}${if (dateMatch) "</b>" else ""}</li>"
+                )
             }
             return MyApplication.application.resources.getString(
                 R.string.lpl_match,
