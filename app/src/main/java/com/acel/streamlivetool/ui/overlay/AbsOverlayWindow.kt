bd7ad567ecaa6ece5edf881be15bc15937534675
acel <clanvick@gmail.com>
2020-12-28 18:54:29 +0800

    修改悬浮窗播放器移动逻辑

--- app/src/main/java/com/acel/streamlivetool/ui/overlay/AbsOverlayWindow.kt
+++ app/src/main/java/com/acel/streamlivetool/ui/overlay/AbsOverlayWindow.kt
@@ -15,7 +15,7 @@ import kotlin.math.abs
 abstract class AbsOverlayWindow {
     private val applicationContext = MyApplication.application.applicationContext
     private val windowManager =
-        MyApplication.application.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            MyApplication.application.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     private var outMetrics = DisplayMetrics().also { windowManager.defaultDisplay.getMetrics(it) }
     private var widthPixels = outMetrics.widthPixels
     private var heightPixels = outMetrics.heightPixels
@@ -30,7 +30,7 @@ abstract class AbsOverlayWindow {
 
 
     init {
-//        ConfigurationChangeBroadcastReceiver().register(applicationContext)
+        ConfigurationChangeBroadcastReceiver().register(applicationContext)
     }
 
     @Suppress("DEPRECATION")
@@ -48,7 +48,7 @@ abstract class AbsOverlayWindow {
         layoutParams.x = x
         layoutParams.y = y
         layoutParams.flags =
-            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         this.mLayout = layout
         show()
         return this
@@ -66,7 +66,7 @@ abstract class AbsOverlayWindow {
 
     fun remove() {
         val windowManager =
-            applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                applicationContext.getSystemService(Context.WINDOW_SERVICE) as WindowManager
         if (isShown) {
             windowManager.removeView(mLayout)
             isShown = false
@@ -78,63 +78,97 @@ abstract class AbsOverlayWindow {
     }
 
     protected fun dp2px(dp: Float): Float =
-        (0.5f + dp * Resources.getSystem().displayMetrics.density)
+            (0.5f + dp * Resources.getSystem().displayMetrics.density)
 
 
     fun setMovable() {
         mLayout.setOnTouchListener(object : View.OnTouchListener {
-            private var x = 0
-            private var y = 0
-            private var xOffset = 0
-            private var yOffset = 0
-            private var downX = 0
-            private var downY = 0
-            override fun onTouch(p0: View, event: MotionEvent): Boolean {
+            var lastRawX = 0f
+            var lastRawY = 0f
+            var downX = 0f
+            var downY = 0f
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
                 when (event.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        x = event.rawX.toInt()
-                        y = event.rawY.toInt()
-                        xOffset = event.x.toInt()
-                        yOffset = event.y.toInt()
-                        downX = event.rawX.toInt()
-                        downY = event.rawY.toInt()
+                        lastRawX = event.rawX
+                        lastRawY = event.rawY
+                        downX = event.rawX
+                        downY = event.rawY
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        val nowX = event.rawX
-                        val nowY = event.rawY
-                        if (nowX >= 0 && nowY >= 0) {
-                            val movedX = nowX - x
-                            val movedY = nowY - y
-                            var newX = layoutParams.x + movedX
-                            //如果越界，移动到X最大
-                            if (newX > widthPixels - mLayout.width)
-                                newX = (widthPixels - mLayout.width).toFloat()
-                            var newY = layoutParams.y + movedY
-                            //如果越界，移动到Y最大
-                            if (newY > heightPixels - mLayout.height)
-                                newY = (heightPixels - mLayout.height).toFloat()
-                            val checkX =
-                                newX >= 0 && newX <= widthPixels - mLayout.width
-                            if (checkX) {
-                                layoutParams.x = (newX).toInt()
-                                x = nowX.toInt()
-                            }
-                            val checkY =
-                                newY >= 0 && newY <= heightPixels - mLayout.height
-                            if (checkY) {
-                                layoutParams.y = (newY).toInt()
-                                y = nowY.toInt()
-                            }
-                            // 更新悬浮窗控件布局
-                            if (checkX || checkY)
-                                windowManager.updateViewLayout(p0, layoutParams)
-                        }
+                        val offsetX = event.rawX - lastRawX
+                        val offsetY = event.rawY - lastRawY
+                        layoutParams.x += offsetX.toInt()
+                        layoutParams.y += offsetY.toInt()
+                        lastRawX += offsetX
+                        lastRawY += offsetY
+                        windowManager.updateViewLayout(view, layoutParams)
                     }
                     MotionEvent.ACTION_UP -> {
+                        layoutParams.apply {
+                            when {
+                                x < 0 ->
+                                    x = 0
+                                x > widthPixels - mLayout.width ->
+                                    x = widthPixels - mLayout.width
+                            }
+                            when {
+                                y < (0 - getStatusBarHeight()) ->
+                                    y = 0 - getStatusBarHeight()
+                                y > heightPixels - mLayout.height ->
+                                    y = heightPixels - mLayout.height - getStatusBarHeight()
+                            }
+                        }
+                        windowManager.updateViewLayout(view, layoutParams)
                         if (abs(event.rawX - downX) < 5 && abs(event.rawY - downY) < 5)
-                            p0.performClick()
+                            view.performClick()
                     }
                 }
+//                when (event.action) {
+//                    MotionEvent.ACTION_DOWN -> {
+//                        x = event.rawX.toInt()
+//                        y = event.rawY.toInt()
+//                        xOffset = event.x.toInt()
+//                        yOffset = event.y.toInt()
+//                        downX = event.rawX.toInt()
+//                        downY = event.rawY.toInt()
+//                    }
+//                    MotionEvent.ACTION_MOVE -> {
+//                        val nowX = event.rawX
+//                        val nowY = event.rawY
+//                        if (nowX >= 0 && nowY >= 0) {
+//                            val movedX = nowX - x
+//                            val movedY = nowY - y
+//                            var newX = layoutParams.x + movedX
+//                            //如果越界，移动到X最大
+//                            if (newX > widthPixels - mLayout.width)
+//                                newX = (widthPixels - mLayout.width).toFloat()
+//                            var newY = layoutParams.y + movedY
+//                            //如果越界，移动到Y最大
+//                            if (newY > heightPixels - mLayout.height)
+//                                newY = (heightPixels - mLayout.height).toFloat()
+//                            val checkX =
+//                                newX >= 0 && newX <= widthPixels - mLayout.width
+//                            if (checkX) {
+//                                layoutParams.x = (newX).toInt()
+//                                x = nowX.toInt()
+//                            }
+//                            val checkY =
+//                                newY >= 0 && newY <= heightPixels - mLayout.height
+//                            if (checkY) {
+//                                layoutParams.y = (newY).toInt()
+//                                y = nowY.toInt()
+//                            }
+//                            // 更新悬浮窗控件布局
+//                            if (checkX || checkY)
+//                                windowManager.updateViewLayout(view, layoutParams)
+//                        }
+//                    }
+//                    MotionEvent.ACTION_UP -> {
+//                        if (abs(event.rawX - downX) < 5 && abs(event.rawY - downY) < 5)
+//                            view.performClick()
+//                    }
+//                }
                 return true
             }
         })
@@ -149,9 +183,15 @@ abstract class AbsOverlayWindow {
 
         fun register(context: Context) {
             context.registerReceiver(
-                ConfigurationChangeBroadcastReceiver(),
-                IntentFilter("android.intent.action.CONFIGURATION_CHANGED")
+                    ConfigurationChangeBroadcastReceiver(),
+                    IntentFilter("android.intent.action.CONFIGURATION_CHANGED")
             )
         }
+    }
+
+    private fun getStatusBarHeight(): Int {
+        val resources = MyApplication.application.resources
+        val resourceId = MyApplication.application.resources.getIdentifier("status_bar_height", "dimen", "android")
+        return resources.getDimensionPixelSize(resourceId)
     }
 }
